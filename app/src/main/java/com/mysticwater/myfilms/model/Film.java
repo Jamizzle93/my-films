4674a94dc96f1d16e890c4f34a683723650d93ca
James Warner <jamesnwarner@gmail.com>
2016-09-29 20:45:45 +0100

    feat(Film): Add Film model object

---
+++ app/src/main/java/com/mysticwater/myfilms/model/Film.java
@@ -0,0 +1,15 @@
+package com.mysticwater.myfilms.model;
+
+import java.util.Calendar;
+
+public class Film {
+
+    private String title;
+    private String description;
+    private Calendar releaseDate;
+
+    public Film(String title) {
+        this.title = title;
+    }
+
+}
