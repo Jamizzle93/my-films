48b22fdb67f0e207e6da31b995a9f16514e6e18d
James Warner <jamesnwarner@gmail.com>
2016-10-02 18:04:21 +0100

    feat(*): Add images to upcoming films list

--- app/src/main/java/com/mysticwater/myfilms/views/adapters/FilmAdapter.java
+++ app/src/main/java/com/mysticwater/myfilms/views/adapters/FilmAdapter.java
@@ -1,7 +1,9 @@
 package com.mysticwater.myfilms.views.adapters;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.text.TextUtils;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,28 +11,30 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jakewharton.picasso.OkHttp3Downloader;
 import com.mysticwater.myfilms.R;
 import com.mysticwater.myfilms.model.Film;
 import com.mysticwater.myfilms.utils.CalendarUtils;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 
 public class FilmAdapter extends ArrayAdapter<Film> {
 
     private Context mContext;
 
-    public FilmAdapter(Context context, List<Film> films)
-    {
+    public FilmAdapter(Context context, List<Film> films) {
         super(context, R.layout.row_upcoming_film, films);
         mContext = context;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
+    public View getView(int position, View convertView, ViewGroup parent) {
         FilmHolder filmHolder;
-        if (convertView == null)
-        {
+        if (convertView == null) {
             filmHolder = new FilmHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.row_upcoming_film, parent, false);
@@ -47,8 +51,7 @@ public class FilmAdapter extends ArrayAdapter<Film> {
         }
 
         Film film = getItem(position);
-        if (film != null)
-        {
+        if (film != null) {
             String title = film.getTitle();
             if (!TextUtils.isEmpty(title)) {
                 filmHolder.title.setText(title);
@@ -63,7 +66,29 @@ public class FilmAdapter extends ArrayAdapter<Film> {
 
             String posterPath = film.getPosterPath();
             if (!TextUtils.isEmpty(posterPath)) {
-                //TODO - Get the poster...
+                // TODO - Update after new release of Picasso
+                String imageUri = mContext.getString(R.string.moviedb_image_w500_url, posterPath);
+
+                OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                        .connectTimeout(10, TimeUnit.SECONDS)
+                        .build();
+
+                Picasso.Builder builder = new Picasso.Builder(mContext);
+
+                int pixelWidth = dpToPixels(100);
+                int pixelHeight = dpToPixels(150);
+
+                builder.downloader(new OkHttp3Downloader(okHttpClient))
+                        .build()
+                        .load(imageUri)
+                        .resize(pixelWidth, pixelHeight)
+                        .centerCrop()
+                        .tag(mContext)
+                        .into(filmHolder.poster);
+            }
+            else
+            {
+                filmHolder.poster.setImageResource(0);
             }
         }
 
@@ -74,6 +99,11 @@ public class FilmAdapter extends ArrayAdapter<Film> {
         ImageView poster;
         TextView title;
         TextView releaseDate;
+    }
+
+    private int dpToPixels(int dp) {
+        Resources r = mContext.getResources();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
     }
 
 }
