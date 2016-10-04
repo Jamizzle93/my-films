ee80d3d7e7cab1eb7636c83c0182afd4e2dde704
James Warner <jamesnwarner@gmail.com>
2016-10-04 21:27:46 +0100

    feat(FilmDetail): Load in backdrop image when opening film detail

--- app/src/main/java/com/mysticwater/myfilms/fragments/FilmDetailFragment.java
+++ app/src/main/java/com/mysticwater/myfilms/fragments/FilmDetailFragment.java
@@ -7,19 +7,25 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jakewharton.picasso.OkHttp3Downloader;
 import com.mysticwater.myfilms.FilmDetailActivity;
 import com.mysticwater.myfilms.R;
 import com.mysticwater.myfilms.model.Film;
 import com.mysticwater.myfilms.utils.filmcontentprovider.FilmColumns;
+import com.squareup.picasso.Picasso;
+
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
 
 import static com.mysticwater.myfilms.utils.filmcontentprovider.FilmsProvider.Films.CONTENT_URI;
 
@@ -65,6 +71,30 @@ public class FilmDetailFragment extends Fragment {
                 actionBar.setTitle(mFilm.getTitle());
             }
 
+            // Load in the backdrop
+            String backdropPath = mFilm.getBackdropPath();
+            if (!TextUtils.isEmpty(backdropPath)) {
+                // TODO - Update after new release of Picasso
+                String imageUri = getString(R.string.moviedb_backdrop_w1280_url, backdropPath);
+
+                OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                        .connectTimeout(10, TimeUnit.SECONDS)
+                        .build();
+
+                Picasso.Builder builder = new Picasso.Builder(getActivity());
+
+                builder.downloader(new OkHttp3Downloader(okHttpClient))
+                        .build()
+                        .load(imageUri)
+                        .tag(getActivity())
+                        .into(filmBackdrop);
+            }
+            else
+            {
+                filmBackdrop.setImageResource(0);
+            }
+
+            // Load the title
             filmTitle.setText(mFilm.getTitle());
         }
 
