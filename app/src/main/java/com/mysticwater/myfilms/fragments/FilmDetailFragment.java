a807137754ba5a6401c2ce1f89b8182bfc7ea67d
James Warner <jamesnwarner@gmail.com>
2016-10-19 21:30:02 +0100

    feat(FilmDetail): Update runtime and calendar intent for film detail

--- app/src/main/java/com/mysticwater/myfilms/fragments/FilmDetailFragment.java
+++ app/src/main/java/com/mysticwater/myfilms/fragments/FilmDetailFragment.java
@@ -4,9 +4,11 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -29,7 +31,10 @@ import com.mysticwater.myfilms.utils.CalendarUtils;
 import com.mysticwater.myfilms.utils.filmcontentprovider.FilmsDbHelper;
 import com.mysticwater.myfilms.utils.filmcontentprovider.FilmsProvider;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -58,8 +63,12 @@ public class FilmDetailFragment extends Fragment {
     TextView filmGenres;
     @BindView(R.id.film_rating_text)
     TextView filmRating;
+    @BindView(R.id.film_runtime_text)
+    TextView filmRuntime;
     @BindView(R.id.film_imdb_button)
     ImageView imdbButton;
+    @BindView(R.id.film_add_event)
+    ImageView addEventButton;
 
     // Bundle strings
     public static final String FILM_ID = "FilmId";
@@ -138,6 +147,24 @@ public class FilmDetailFragment extends Fragment {
                     .getReleaseDate());
             filmReleaseDate.setText(releaseDate);
 
+            final Calendar releaseDateCal = CalendarUtils.dateStringToCalendar(mFilm.getReleaseDate());
+
+            // Update the calendar button
+            addEventButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_INSERT)
+                            .setData(CalendarContract.Events.CONTENT_URI)
+                            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, releaseDateCal.getTimeInMillis())
+                            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, releaseDateCal.getTimeInMillis())
+                            .putExtra(CalendarContract.Events.TITLE, mFilm.getTitle())
+                            .putExtra(CalendarContract.Events.ALL_DAY, true)
+                            .putExtra(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getTimeZone("UTC"));
+                    startActivity(intent);
+
+                }
+            });
+
             // Handle favouriting
             favouriteFilm.setFavorite(FilmsDbHelper.isFilmFavourite(getActivity(), mFilm));
             favouriteFilm.setOnFavoriteChangeListener(
@@ -184,6 +211,19 @@ public class FilmDetailFragment extends Fragment {
             else
             {
                 imdbButton.setVisibility(View.GONE);
+            }
+
+            if (mFilm.getRuntime() != null)
+            {
+                int runtime = mFilm.getRuntime();
+                int hours = (runtime / 60);
+                int minutes = (runtime % 60);
+
+                String runtimeHours  = getResources().getQuantityString(R.plurals.hours, hours,
+                    hours);
+                String runtimeMins = getResources().getQuantityString(R.plurals.minutes, minutes,
+                        minutes);
+                filmRuntime.setText(runtimeHours + " " + runtimeMins);
             }
 
         }
