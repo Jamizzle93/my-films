a3946c7c24616cfb0eeecf607c0e770f241fc9da
James Warner <jamesnwarner@gmail.com>
2016-10-22 19:26:40 +0100

    wip(*): Add layout and activity which loads search film results

--- app/src/main/java/com/mysticwater/myfilms/SearchResultsActivity.java
+++ app/src/main/java/com/mysticwater/myfilms/SearchResultsActivity.java
@@ -4,17 +4,73 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 
+import com.crashlytics.android.Crashlytics;
+import com.mysticwater.myfilms.model.Film;
+import com.mysticwater.myfilms.model.FilmResults;
+import com.mysticwater.myfilms.network.TheMovieDbService;
+import com.mysticwater.myfilms.utils.filmcontentprovider.FilmsDbHelper;
+import com.mysticwater.myfilms.views.adapters.FilmAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SearchResultsActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "SearchResultsActivity";
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.swipe_refresh_search)
+    SwipeRefreshLayout mSwipeRefresh;
+    @BindView(R.id.search_films_list)
+    RecyclerView mFilmsRecyclerView;
+
+    private List<Film> mFilms;
+    private FilmAdapter mFilmsAdapter;
+
+    private String mQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_search_result);
+        ButterKnife.bind(this);
+
         handleIntent(getIntent());
+
+        mToolbar.setTitle("Search Results");
+
+        // Setup recycler view
+        mFilms = new ArrayList<>();
+        mFilmsAdapter = new FilmAdapter(this, mFilms);
+        mFilmsRecyclerView.setAdapter(mFilmsAdapter);
+        mFilmsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mSwipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Crashlytics.log(Log.INFO, LOG_TAG, "onRefresh called from SwipeRefreshLayout");
+
+                searchFilms(mQuery);
+            }
+        });
     }
 
     @Override
@@ -38,10 +94,47 @@ public class SearchResultsActivity extends AppCompatActivity {
     private void handleIntent(Intent intent) {
 
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            System.out.println(query);
-            //use the query to search
+            mQuery = intent.getStringExtra(SearchManager.QUERY);
+            searchFilms(mQuery);
         }
+    }
+
+    private void searchFilms(String query) {
+        TheMovieDbService theMovieDbService = TheMovieDbService.retrofit.create(TheMovieDbService.class);
+
+        final Call<FilmResults> searchFilms = theMovieDbService.searchFilms(getString(R.string
+                .moviedb_api_key), query);
+
+        searchFilms.enqueue(new Callback<FilmResults>() {
+            @Override
+            public void onResponse(Call<FilmResults> call, Response<FilmResults> response) {
+                mSwipeRefresh.setRefreshing(false);
+
+                FilmResults films = response.body();
+                if (films != null) {
+                    for (Film film : films.getFilms()) {
+                        // Only care about films with a poster and backdrop
+                        if (!TextUtils.isEmpty(film.getPosterPath()) && !TextUtils.isEmpty(film.getBackdropPath())) {
+                            mFilms.add(film);
+                        }
+                    }
+                    fillList();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FilmResults> call, Throwable t) {
+                Crashlytics.log(Log.ERROR, LOG_TAG, "Failed to get response for upcoming releases. " + t.getMessage());
+
+                mSwipeRefresh.setRefreshing(false);
+
+                fillList();
+            }
+        });
+    }
+
+    private void fillList() {
+        mFilmsAdapter.updateData(mFilms);
     }
 
 }
